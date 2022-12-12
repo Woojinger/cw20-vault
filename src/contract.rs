00be9c78ce1bb48c9a3f56c850fc06eda485eba1
woojinger <tlsdnwls1253@gmail.com>
2022-12-12 23:13:47 +0900

    Implement ExecuteMsg::CreateVault and add test case

--- src/contract.rs
+++ src/contract.rs
@@ -53,19 +53,19 @@ pub fn execute(
         ExecuteMsg::CreateVault() => {
             let vault = Vault {
                 admin_addr: Addr::unchecked(info.sender),
-                collected: Uint128::new(10),
-                ledger_list: vec![Ledger { coin_amount: Uint128::new(10), receive_time: Timestamp::from_seconds(10) }],
+                collected: Uint128::new(0),
+                ledger_list: vec![],
             };
             let newVaultId = save_vault(deps, &vault).unwrap();
             Ok(Response::new()
-                .add_attribute("method","execute_create_vault")
-                .add_attribute("vault_id",Uint64::new(newVaultId)))
+                .add_attribute("method", "execute_create_vault")
+                .add_attribute("vault_id", Uint64::new(newVaultId)))
         }
         ExecuteMsg::Withdraw { vaultId: vaultId, amount: amount } => {
             // TODO
             Ok(Response::new())
         }
-        ExecuteMsg::Receive ( msg ) => {
+        ExecuteMsg::Receive(msg) => {
             // TODO
             Ok(Response::new())
         }
@@ -105,8 +105,9 @@ mod tests {
 
         let _res = instantiate(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
 
-        let msg = ExecuteMsg::CreateVault ();
-        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        // create 1st vault
+        let msg = ExecuteMsg::CreateVault();
+        let res = execute(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
         assert_eq!(res.attributes.get(1).unwrap().value, "1");
 
         let msg = QueryMsg::GetVault { id: Uint64::new(1) };
@@ -117,8 +118,26 @@ mod tests {
             vault,
             Vault {
                 admin_addr: Addr::unchecked("tx_sender"),
-                collected: Uint128::new(10),
-                ledger_list: vec![Ledger { coin_amount: Uint128::new(10), receive_time: Timestamp::from_seconds(10) }],
+                collected: Uint128::new(0),
+                ledger_list: vec![],
+            }
+        );
+
+        // create 2nd vault
+        let msg = ExecuteMsg::CreateVault();
+        let res = execute(deps.as_mut(), mock_env(), info.clone(), msg).unwrap();
+        assert_eq!(res.attributes.get(1).unwrap().value, "2");
+
+        let msg = QueryMsg::GetVault { id: Uint64::new(2) };
+        let res = query(deps.as_ref(), mock_env(), msg).unwrap();
+
+        let vault: Vault = from_binary(&res).unwrap();
+        assert_eq!(
+            vault,
+            Vault {
+                admin_addr: Addr::unchecked("tx_sender"),
+                collected: Uint128::new(0),
+                ledger_list: vec![],
             }
         );
     }
